09b56cf85540bdcb0840dd00da0bbe5ca8b48abf
Gustavo-CS <gustavocarvalho9340@gmail.com>
2024-09-14 17:33:15 -0300

    colocando a leitura dos tokens na tabela de simbolos

--- analisador.c
+++ analisador.c
@@ -14,6 +14,11 @@ char *prog;
 char token[80];
 char token_type;
 
+//utilizado para verificar a coluna e linha do token
+int line = 1;
+int column = 1;
+int columnAux = 1;
+
 // Protótipo das funções
 int isdelim(char c);
 void skip_whitespace_and_comments(void);
@@ -36,6 +41,7 @@ void skip_whitespace_and_comments(void)
         {
             // Ignora espaços, tabulações e quebras de linha
             ++prog;
+            column++;
         }
         else
         {
@@ -54,6 +60,12 @@ void get_token(void)
     temp = token;
     *temp = '\0';
 
+    if (*prog == '\n')
+    {
+        line++;
+        column = 0;
+    }
+
     if (!*prog)
         return;
 
@@ -63,26 +75,34 @@ void get_token(void)
     {
         token_type = DELIMITADOR;
         *temp++ = *prog++;
+        columnAux = column;
+        column++;
     }
     else if (isalpha(*prog))
     {
+        columnAux = column;
         while (!isdelim(*prog) && *prog != '\0')
         {
             *temp++ = *prog++;
+            column++;
         }
+
         *temp = '\0';
         token_type = (strcmp(token, "var") == 0 || strcmp(token, "begin") == 0 || strcmp(token, "end") == 0 || strcmp(token, "if") == 0 || strcmp(token, "then") == 0 || strcmp(token, "else") == 0 || strcmp(token, "while") == 0 || strcmp(token, "do") == 0) ? PALAVRA_CHAVE : VARIAVEL;
     }
     else if (isdigit(*prog) || (*prog == '.' && isdigit(*(prog + 1))))
     {
+        columnAux = column;
         while (!isdelim(*prog) && *prog != '\0')
         {
             *temp++ = *prog++;
+            column++;
         }
         token_type = NUMERO;
     }
     else
     {
+        columnAux = column;
         token_type = ERRO;
         *temp++ = *prog++;
     }
@@ -90,26 +110,65 @@ void get_token(void)
 }
 
 // Função para exibir o token e seu tipo
+// void print_token(void) {
+//     switch (token_type) {
+//         case DELIMITADOR:
+//             printf("DELIMITADOR: %s\n", token);
+//             break;
+//         case VARIAVEL:
+//             printf("VARIAVEL: %s\n", token);
+//             break;
+//         case NUMERO:
+//             printf("NUMERO: %s\n", token);
+//             break;
+//         case PALAVRA_CHAVE:
+//             printf("PALAVRA CHAVE: %s\n", token);
+//             break;
+//         default:
+//             printf("ERRO: %s\n", token);
+//             break;
+//     }
+// }
+
+//modificado para ao invés de printar no console, armazenar os tokens na tabela de simbolos
 void print_token(void)
 {
+    char tmp[80];
+    FILE *ts = fopen("ts.lex", "a");
+    // fclose(ts);
+    // ts = fopen("ts.lex", "a");
     switch (token_type)
     {
     case DELIMITADOR:
-        printf("DELIMITADOR: %s\n", token);
+        strcpy(tmp, token);
+        sprintf(tmp, "<DELIMITADOR, \"%s\", %d, %d>\n", token, line, columnAux);
+        fputs(tmp, ts);
+        // printf("DELIMITADOR: %s\n", token);
         break;
     case VARIAVEL:
-        printf("VARIAVEL: %s\n", token);
+        strcpy(tmp, token);
+        sprintf(tmp, "<VARIAVEL, \"%s\", %d, %d>\n", token, line, columnAux);
+        fputs(tmp, ts);
+        // printf("VARIAVEL: %s\n", token);
         break;
     case NUMERO:
-        printf("NUMERO: %s\n", token);
+        strcpy(tmp, token);
+        sprintf(tmp, "<NUMERO, \"%s\", %d, %d>\n", token, line, columnAux);
+        fputs(tmp, ts);
+        // printf("NUMERO: %s\n", token);
         break;
     case PALAVRA_CHAVE:
-        printf("PALAVRA CHAVE: %s\n", token);
+        strcpy(tmp, token);
+        sprintf(tmp, "<PALAVRA_CHAVE, \"%s\", %d, %d>\n", token, line, columnAux);
+        fputs(tmp, ts);
+        // printf("PALAVRA CHAVE: %s\n", token);
         break;
     default:
-        printf("ERRO: %s\n", token);
+        printf("<ERRO, \"%s\", %d, %d>\n", token, line, columnAux);
+        // printf("ERRO: %s\n", token);
         break;
     }
+    fclose(ts);
 }
 
 // Função para ler o conteúdo de um arquivo
@@ -148,6 +207,17 @@ void read_file(const char *filename)
 
 int main(void)
 {
+    //inicia o arquivo ts.lex (tabela de simbolos) e copia os itens do arquivo pl_rs.lex (palavras reservadas)
+    //como pedido no trabalho
+    //ainda precisa de alterações como ler as palavras reservadas dele, e a verificação de pre-existencia de tokens antes da criação
+    FILE *ts = fopen("ts.lex", "w");
+    char ch;
+    FILE *source = fopen("pl_rs.lex", "r");
+    while ((ch = fgetc(source)) != EOF)
+        fputc(ch, ts);
+    fclose(source);
+    fclose(ts);
+
     // Nome do arquivo a ser lido
     const char *filename = "lexico.txt";
 
