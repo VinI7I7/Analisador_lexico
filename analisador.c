0dd5d7e059de50f80912ccab60695740896c100f
Gustavo-CS <gustavocarvalho9340@gmail.com>
2024-09-14 16:35:56 -0300

    identacao

--- analisador.c
+++ analisador.c
@@ -22,17 +22,23 @@ void print_token(void);
 void read_file(const char *filename);
 
 // Função para verificar se um caractere é um delimitador
-int isdelim(char c) {
+int isdelim(char c)
+{
     return strchr("+-*/%=() ,;:.\t\r\n<>", c) != NULL;
 }
 
 // Função para ignorar espaços em branco e comentários
-void skip_whitespace_and_comments(void) {
-    while (*prog) {
-        if (isspace(*prog)) {
+void skip_whitespace_and_comments(void)
+{
+    while (*prog)
+    {
+        if (isspace(*prog))
+        {
             // Ignora espaços, tabulações e quebras de linha
             ++prog;
-        } else {
+        }
+        else
+        {
             // Se não for espaço, pare
             break;
         }
@@ -40,64 +46,78 @@ void skip_whitespace_and_comments(void) {
 }
 
 // Função para obter o próximo token
-void get_token(void) {
+void get_token(void)
+{
     register char *temp;
 
     token_type = 0;
     temp = token;
     *temp = '\0';
 
-    if (!*prog) return;
-    
+    if (!*prog)
+        return;
+
     skip_whitespace_and_comments(); // Ignora espaços e comentários antes de processar o token
 
-    if (strchr("+-*/%=() ,;:.<>{}[]", *prog)) {
+    if (strchr("+-*/%=() ,;:.<>{}[]", *prog))
+    {
         token_type = DELIMITADOR;
         *temp++ = *prog++;
-    } else if (isalpha(*prog)) {
-        while (!isdelim(*prog) && *prog != '\0') {
+    }
+    else if (isalpha(*prog))
+    {
+        while (!isdelim(*prog) && *prog != '\0')
+        {
             *temp++ = *prog++;
         }
         *temp = '\0';
         token_type = (strcmp(token, "var") == 0 || strcmp(token, "begin") == 0 || strcmp(token, "end") == 0 || strcmp(token, "if") == 0 || strcmp(token, "then") == 0 || strcmp(token, "else") == 0 || strcmp(token, "while") == 0 || strcmp(token, "do") == 0) ? PALAVRA_CHAVE : VARIAVEL;
-    } else if (isdigit(*prog) || (*prog == '.' && isdigit(*(prog + 1)))) {
-        while (!isdelim(*prog) && *prog != '\0') {
+    }
+    else if (isdigit(*prog) || (*prog == '.' && isdigit(*(prog + 1))))
+    {
+        while (!isdelim(*prog) && *prog != '\0')
+        {
             *temp++ = *prog++;
         }
         token_type = NUMERO;
     }
-    else{
-    	token_type = ERRO;
-    	*temp++ = *prog++;
-	}
+    else
+    {
+        token_type = ERRO;
+        *temp++ = *prog++;
+    }
     *temp = '\0';
 }
 
 // Função para exibir o token e seu tipo
-void print_token(void) {
-    switch (token_type) {
-        case DELIMITADOR:
-            printf("DELIMITADOR: %s\n", token);
-            break;
-        case VARIAVEL:
-            printf("VARIAVEL: %s\n", token);
-            break;
-        case NUMERO:
-            printf("NUMERO: %s\n", token);
-            break;
-        case PALAVRA_CHAVE:
-            printf("PALAVRA CHAVE: %s\n", token);
-            break;
-        default:
-            printf("ERRO: %s\n", token);
-            break;
+void print_token(void)
+{
+    switch (token_type)
+    {
+    case DELIMITADOR:
+        printf("DELIMITADOR: %s\n", token);
+        break;
+    case VARIAVEL:
+        printf("VARIAVEL: %s\n", token);
+        break;
+    case NUMERO:
+        printf("NUMERO: %s\n", token);
+        break;
+    case PALAVRA_CHAVE:
+        printf("PALAVRA CHAVE: %s\n", token);
+        break;
+    default:
+        printf("ERRO: %s\n", token);
+        break;
     }
 }
 
 // Função para ler o conteúdo de um arquivo
-void read_file(const char *filename) {
+void read_file(const char *filename)
+{
     FILE *file = fopen(filename, "r");
-    if (file == NULL) {
+    if (file == NULL)
+    {
         perror("Erro ao abrir o arquivo");
         exit(EXIT_FAILURE);
     }
@@ -109,7 +129,8 @@ void read_file(const char *filename) {
 
     // Aloca buffer para ler o arquivo
     char *buffer = (char *)malloc(file_size + 1);
-    if (buffer == NULL) {
+    if (buffer == NULL)
+    {
         perror("Erro ao alocar memória");
         exit(EXIT_FAILURE);
     }
@@ -125,7 +146,8 @@ void read_file(const char *filename) {
     prog = buffer;
 }
 
-int main(void) {
+int main(void)
+{
     // Nome do arquivo a ser lido
     const char *filename = "lexico.txt";
 
@@ -133,9 +155,11 @@ int main(void) {
     read_file(filename);
 
     // Analisando a expressão
-    while (*prog) {
+    while (*prog)
+    {
         get_token();
-        if (token_type != 0) {
+        if (token_type != 0)
+        {
             print_token();
         }
     }
